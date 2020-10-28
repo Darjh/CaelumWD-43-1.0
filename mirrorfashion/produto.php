d58fa6b65d2cd2a64d288f4556376f8e3f07884a
GhBgmNyP8uJjNmG <darlanpontes.almeida1800@gmail.com>
2020-10-27 22:57:22 -0400

    Atualização!

---
+++ mirrorfashion/produto.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+       
+    </head>
+    <body>
+        <?php 
+            $cabecalho_css = '<link rel="stylesheet" href="css/produto.css">';
+            $cabecalho_title = "Produto da Mirror Fashion";
+            include("cabecalho.php"); 
+        ?>
+
+        <?php include("rodape.php"); ?>
+    </body>
+</html>
